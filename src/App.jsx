09e97dd2fef1f7f8c9798cfae92091474e41a32a
houssamelm <houssamelmasoudi@gmail.com>
2023-01-13 12:09:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState,useEffect, useWindowDimensions } from 'react'
+import reactLogo from './assets/react.svg'
+import Advice from './components/Advice'
+import './App.css'
+
+function App() {
+
+  
+
+  const [advice, setAdvice] = useState({})
+  const [refresh, setRefresh] = useState(true)
+
+
+
+  const generateAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+    .then((response) => response.json())
+    .then((data) => {
+      const adv = data.slip.advice
+      const id  = data.slip.id
+      setAdvice({advice:adv,id:id})
+    })
+    
+  }
+
+  useEffect(() => {
+    generateAdvice()
+  },[])
+
+  return (
+    <div className="App">
+    <Advice advice={advice} generate={generateAdvice}/>
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Advice.jsx
@@ -0,0 +1,48 @@
+import '../App.css'
+import { useState } from 'react';
+
+const Advice = ({ advice, generate }) => {
+
+  const [spin, setSpin] = useState(false);
+
+  const animate = () => {
+
+    // Button begins to spin
+    setSpin(true);
+
+    // Buttons stops to spin after 2 seconds
+    setTimeout(() => setSpin(false), 2000);
+
+  }
+
+  const buttonClick = () => {
+    generate()
+    // Button begins to spin
+    setSpin(true);
+
+    // Buttons stops to spin after 2 seconds
+    setTimeout(() => setSpin(false), 2000);
+  }
+
+  return (
+    <>
+      <div className='container'>
+        <div className='advice'>
+          <p className='adviceNumber'>ADVICE #{advice.id}</p>
+          <p>"{advice.advice}"</p>
+        </div>
+
+        <div className='separator'>
+
+        </div>
+      </div>
+      <div className='buttonContainer'>
+          <button onClick={buttonClick} className={spin ? `spin` : null}>
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733" /></svg>
+          </button>
+      </div>
+    </>
+  )
+}
+
+export default Advice
